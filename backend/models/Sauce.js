eb773fb865b5c886443d27c0e44e1fa15d78c592
jokoul <74833985+jokoul@users.noreply.github.com>
2021-11-23 17:32:30 +0100

    end of the procject v4

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -9,22 +9,22 @@ const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
   name: {
     type: String,
-    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'-]+$/,
+    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'_-]+$/,
     required: true,
   },
   manufacturer: {
     type: String,
-    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'-]+$/,
+    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'_-]+$/,
     required: true,
   },
   description: {
     type: String,
-    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'-]+$/,
+    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'_-]+$/,
     required: true,
   },
   mainPepper: {
     type: String,
-    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'-]+$/,
+    match: /^[a-zA-Z0-9 éèêâäçûüîïôö'_-]+$/,
     required: true,
   },
   imageUrl: { type: String, required: true },
